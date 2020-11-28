32b714b40bf52697a41001649b157a8a4aaf9ced
ruaktvp <vasily.kotov@raiffeisen.ru>
2020-11-28 12:13:34 +0300

    добавлена проверка что товар имее только один стикер

--- src/test/java/StickerTest.java
+++ src/test/java/StickerTest.java
@@ -1,14 +1,20 @@
+import com.sun.source.tree.AssertTree;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-public class StickerTest extends BaseTest{
+import static org.junit.Assert.assertTrue;
+
+public class StickerTest extends BaseTest {
     @Test
-    public void stickerTest(){
-        driver.navigate().to("http://192.168.64.2/litecart/en/");
+    public void stickerTest() {
+        driver.navigate().to("http://192.168.64.3/litecart/en/");
         List<WebElement> list = driver.findElements(By.xpath("//li[@class='product column shadow hover-light']"));
-        list.forEach(webElement -> webElement.findElement(By.xpath(".//div[contains(@class,'sticker ')]")));
+        list.forEach(
+                webElement -> assertTrue(
+                        webElement.findElements(By.xpath(".//div[contains(@class,'sticker ')]")).size() == 1
+                ));
     }
 }
